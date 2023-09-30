28f3a6926bd5743bf74ea1bdefddb80a559d39ae
Rajit Deb <rajitdeb456@gmail.com>
2023-09-30 12:08:46 +0530

    Developed SelectGameDifficultyFragment and implemented OkHttpClient and LoggingInterceptor

--- app/src/main/java/com/rajit/triviaapp/data/network/api/ApiService.kt
+++ app/src/main/java/com/rajit/triviaapp/data/network/api/ApiService.kt
@@ -1,12 +1,22 @@
 package com.rajit.triviaapp.data.network.api
 
 import com.rajit.triviaapp.data.network.model.AllCategory
+import com.rajit.triviaapp.data.network.model.AllQuestions
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("/api_category.php")
     fun getCategories(): Observable<AllCategory>
+
+    @GET("/api.php")
+    fun getQuestions(
+        @Query("amount") amount: Int = 15,
+        @Query("category") category: Int,
+        @Query("difficulty") difficulty: String,
+        @Query("type") type: String = "multiple"
+    ): Observable<AllQuestions>
 
 }
--- app/src/main/java/com/rajit/triviaapp/data/repository/Repository.kt
+++ app/src/main/java/com/rajit/triviaapp/data/repository/Repository.kt
@@ -2,6 +2,7 @@ package com.rajit.triviaapp.data.repository
 
 import com.rajit.triviaapp.data.network.api.ApiService
 import com.rajit.triviaapp.data.network.model.AllCategory
+import com.rajit.triviaapp.data.network.model.AllQuestions
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
@@ -10,7 +11,20 @@ class Repository (
 ) {
 
     fun getCategories(): Observable<AllCategory> {
-        return triviaApi.getCategories().subscribeOn(Schedulers.io())
+        return triviaApi
+            .getCategories()
+            .subscribeOn(Schedulers.io())
+    }
+
+    /**
+     * This function is used to get questions list from the API based on category Id and difficulty level
+     * @param categoryId is the id of the category, selected by the USER
+     * @param difficulty is the difficulty level, selected by the USER
+     */
+    fun getQuestions(categoryId: Int, difficulty: String): Observable<AllQuestions> {
+        return triviaApi
+            .getQuestions(category = categoryId, difficulty = difficulty)
+            .subscribeOn(Schedulers.io())
     }
 
 }
